f6ffe875fef8cd50eed75b86b7034bda6c6199c3
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-14 01:47:54 +0200

    fix: include node number constraint into the algorithm

--- src/solve.cpp
+++ src/solve.cpp
@@ -2,46 +2,28 @@
 #include "solve_random.cpp"
 #include "types.cpp"
 
-#include <algorithm>
-#include <cmath>
-#include <numeric>
 #include <vector>
 
 enum heuristic_t { RANDOM, NN_END, NN_ANY };
 
-std::vector<unsigned int> constrain(const tsp_t &tsp, unsigned int start) {
-  int n = ceil(tsp.n / 2);
-  adj_list_t neighbors = tsp.adj_matrix[start];
-  std::vector<unsigned int> indices(neighbors.size());
-  std::iota(indices.begin(), indices.end(), 0);
-
-  std::stable_sort(indices.begin(), indices.end(),
-                   [&neighbors](unsigned int a, unsigned int b) {
-                     return neighbors[a] < neighbors[b];
-                   });
-
-  return std::vector<unsigned int>(indices.begin(), indices.begin() + n);
-}
-
 std::vector<solution_t> solve(const tsp_t &tsp, heuristic_t heuristic) {
   std::vector<solution_t> solutions;
+  unsigned int n = ceil(tsp.n / 2.0);
 
   if (heuristic == RANDOM) {
-    return solve_random(tsp);
+    return solve_random(tsp, n);
   }
 
   for (unsigned int i = 0; i < tsp.n; i++) {
-    std::vector<unsigned int> indices = constrain(tsp, i);
-
     switch (heuristic) {
     case RANDOM:
       // NOTE: should never reach this point, but it's here to avoid a warning
       break;
     case NN_END:
-      solutions.push_back(solve_nn_end(tsp, indices));
+      solutions.push_back(solve_nn_end(tsp, n, i));
       break;
     case NN_ANY:
-      solutions.push_back(solve_nn_any(tsp, indices));
+      solutions.push_back(solve_nn_any(tsp, n, i));
       break;
     }
   }
