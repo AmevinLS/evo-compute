34fb82bbee512f51005fb1fd6e8eec0c2852de39
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-13 16:50:04 +0200

    fix: parse the file just once

--- src/parse.cpp
+++ src/parse.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
 #include <cmath>
 #include <fstream>
-#include <iterator>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -9,7 +7,7 @@
 #include "types.cpp"
 
 int l2(const node_t a, const node_t b) {
-  return int(round(sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2))));
+  return int(round(sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))));
 }
 
 adj_matrix_t matrixof(const std::vector<node_t> nodes) {
@@ -18,7 +16,6 @@ adj_matrix_t matrixof(const std::vector<node_t> nodes) {
   for (unsigned int i = 0; i < nodes.size(); i++) {
     for (unsigned int j = 0; j < nodes.size(); j++) {
       if (i == j) {
-        matrix[i][j] = 0;
         continue;
       }
 
@@ -30,11 +27,9 @@ adj_matrix_t matrixof(const std::vector<node_t> nodes) {
 }
 
 std::vector<node_t> read(std::ifstream &in) {
-  int n = std::count(std::istreambuf_iterator<char>(in),
-                     std::istreambuf_iterator<char>(), '\n');
-  std::vector<node_t> nodes(n);
-
+  std::vector<node_t> nodes;
   std::string line;
+
   while (std::getline(in, line)) {
     std::stringstream ss(line);
     std::string value;
