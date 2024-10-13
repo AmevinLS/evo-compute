37fb26619f7c33c82eccbf461c077bfa813aaa03
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-13 15:37:25 +0200

    fix: replace row type with a typedef to vector<int> as adj_list_t

--- src/types.cpp
+++ src/types.cpp
@@ -6,20 +6,16 @@ struct node_t {
   int weight;
 };
 
+typedef std::vector<int> adj_list_t;
+
 struct adj_matrix_t {
   std::vector<std::vector<int>> m;
 
   adj_matrix_t(unsigned int n) : m(n, std::vector<int>(n, 0)) {}
 
-  struct row_t {
-    std::vector<int> &r;
-    row_t(std::vector<int> &r) : r(r) {}
-    int &operator[](unsigned int j) { return r.at(j); }
-  };
-
   int &operator()(unsigned int i, unsigned int j) { return m.at(i).at(j); }
 
-  row_t operator[](unsigned int i) { return row_t(m.at(i)); }
+  adj_list_t operator[](unsigned int i) { return adj_list_t(m.at(i)); }
 };
 
 struct tsp_t {
