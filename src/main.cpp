1def95adc2a5323fe0cb58e7de53e66013bc5e53
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-13 17:54:34 +0200

    feat: add a solve command

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 
 #define ERROR "\033[0;31m[ERROR]\033[0m"
 
+enum heuristic_t { RANDOM };
+
 std::optional<std::ifstream> open_file(const std::string &fname) {
   std::ifstream in(fname);
   if (!in.is_open()) {
@@ -26,6 +28,13 @@ int parse_main(int argc, char **argv) {
     return 1;
   }
 
+  if (strcmp(argv[2], "--help") == 0) {
+    std::cout << "usage: " << argv[0] << " parse <file> [options]" << std::endl;
+    std::cout << "options:" << std::endl;
+    std::cout << "\t-v, --verbose\tPrint additional information" << std::endl;
+    return 0;
+  }
+
   std::string fname = argv[2];
   bool verbose = false;
 
@@ -33,10 +42,11 @@ int parse_main(int argc, char **argv) {
   while (++i < argc) {
     if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
       verbose = true;
-    } else {
-      std::cerr << ERROR << " unknown option: " << argv[i] << std::endl;
-      return 1;
+      continue;
     }
+
+    std::cerr << ERROR << " unknown option: " << argv[i] << std::endl;
+    return 1;
   }
 
   auto in = open_file(fname);
@@ -54,6 +64,65 @@ int parse_main(int argc, char **argv) {
     std::cout << std::endl;
     std::cout << "Adjacency matrix:" << std::endl;
     std::cout << tsp.adj_matrix << std::endl;
+  }
+
+  return 0;
+}
+
+int solve_main(int argc, char **argv) {
+  if (argc < 3) {
+    std::cerr << ERROR << " usage: " << argv[0] << " solve <file> [options]"
+              << std::endl;
+    return 1;
+  }
+
+  if (strcmp(argv[2], "--help") == 0) {
+    std::cout << "usage: " << argv[0] << " solve <file> [options]" << std::endl;
+    std::cout << "options:" << std::endl;
+    std::cout << "\t--heuristic string\tHeuristic to use (\"random\") (default "
+                 "\"random\")"
+              << std::endl;
+    return 0;
+  }
+
+  std::string fname = argv[2];
+  heuristic_t heuristic = RANDOM;
+
+  int i = 2;
+  while (++i < argc) {
+    if (strcmp(argv[i], "--heuristic") == 0) {
+      if (i + 1 >= argc) {
+        std::cerr << ERROR << " missing argument for --heuristic" << std::endl;
+        return 1;
+      }
+
+      if (strcmp(argv[i + 1], "random") == 0) {
+        heuristic = RANDOM;
+      } else {
+        std::cerr << ERROR << " unknown heuristic: " << argv[i + 1]
+                  << std::endl;
+        return 1;
+      }
+
+      i++;
+      continue;
+    }
+
+    std::cerr << ERROR << " unknown option: " << argv[i] << std::endl;
+    return 1;
+  }
+
+  auto in = open_file(fname);
+  if (!in.has_value()) {
+    return 1;
+  }
+
+  tsp_t tsp = parse(in.value());
+
+  switch (heuristic) {
+  case RANDOM:
+    std::cout << "Random heuristic" << std::endl;
+    break;
   }
 
   return 0;
@@ -79,6 +148,10 @@ int main(int argc, char **argv) {
 
   if (strcmp(argv[1], "parse") == 0) {
     return parse_main(argc, argv);
+  }
+
+  if (strcmp(argv[1], "solve") == 0) {
+    return solve_main(argc, argv);
   }
 
   std::cerr << ERROR << " unknown command: " << argv[1] << std::endl;
