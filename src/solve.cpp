b93a35bc0f81b8076bdb6e7eb093615782454919
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-14 02:58:37 +0200

    feat: add Nikita's greedy_cycle

--- src/solve.cpp
+++ src/solve.cpp
@@ -1,10 +1,18 @@
+#include "solve_greedy_cycle.cpp"
 #include "solve_nn.cpp"
 #include "solve_random.cpp"
 #include "types.cpp"
 
+#include <map>
 #include <vector>
 
-enum heuristic_t { RANDOM, NN_END, NN_ANY };
+enum heuristic_t { RANDOM, NN_END, NN_ANY, GREEDY_CYCLE };
+
+std::map<heuristic_t, std::string> heuristic_t_str = {
+    {RANDOM, "random"},
+    {NN_END, "nn_end"},
+    {NN_ANY, "nn_any"},
+    {GREEDY_CYCLE, "greedy_cycle"}};
 
 std::vector<solution_t> solve(const tsp_t &tsp, heuristic_t heuristic) {
   std::vector<solution_t> solutions;
@@ -25,6 +33,8 @@ std::vector<solution_t> solve(const tsp_t &tsp, heuristic_t heuristic) {
     case NN_ANY:
       solutions.push_back(solve_nn_any(tsp, n, i));
       break;
+    case GREEDY_CYCLE:
+      solutions.push_back(run_greedy_cycle(tsp, n, i));
     }
   }
 
