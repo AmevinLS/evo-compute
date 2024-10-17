e35884e9f4bef52e11ee2034a72be61859d5941b
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-10-18 00:48:38 +0200

    Feat: cycle-2-regret algo

--- src/solve.cpp
+++ src/solve.cpp
@@ -2,41 +2,49 @@
 #include "task1/solve_greedy_cycle.cpp"
 #include "task1/solve_nn.cpp"
 #include "task1/solve_random.cpp"
+#include "task2/solve_greedy_regret.cpp"
 
 #include <map>
 #include <vector>
 
-enum heuristic_t { RANDOM, NN_END, NN_ANY, GREEDY_CYCLE };
+enum heuristic_t { RANDOM, NN_END, NN_ANY, GREEDY_CYCLE, GREEDY_CYCLE_REGRET };
 
 std::map<heuristic_t, std::string> heuristic_t_str = {
     {RANDOM, "random"},
     {NN_END, "nn_end"},
     {NN_ANY, "nn_any"},
-    {GREEDY_CYCLE, "greedy_cycle"}};
+    {GREEDY_CYCLE, "greedy_cycle"},
+    {GREEDY_CYCLE_REGRET, "greedy_cycle_regret"}};
 
 std::vector<solution_t> solve(const tsp_t &tsp, heuristic_t heuristic) {
-  std::vector<solution_t> solutions;
-  unsigned int n = ceil(tsp.n / 2.0);
+    std::vector<solution_t> solutions;
+    unsigned int n = ceil(tsp.n / 2.0);
 
-  if (heuristic == RANDOM) {
-    return solve_random(tsp, n);
-  }
-
-  for (unsigned int i = 0; i < tsp.n; i++) {
-    switch (heuristic) {
-    case RANDOM:
-      // NOTE: should never reach this point, but it's here to avoid a warning
-      break;
-    case NN_END:
-      solutions.push_back(solve_nn_end(tsp, n, i));
-      break;
-    case NN_ANY:
-      solutions.push_back(solve_nn_any(tsp, n, i));
-      break;
-    case GREEDY_CYCLE:
-      solutions.push_back(run_greedy_cycle(tsp, n, i));
+    if (heuristic == RANDOM) {
+        return solve_random(tsp, n);
     }
-  }
 
-  return solutions;
+    for (unsigned int i = 0; i < tsp.n; i++) {
+        switch (heuristic) {
+        case RANDOM:
+            // NOTE: should never reach this point, but it's here to avoid a
+            // warning
+            break;
+        case NN_END:
+            solutions.push_back(solve_nn_end(tsp, n, i));
+            break;
+        case NN_ANY:
+            solutions.push_back(solve_nn_any(tsp, n, i));
+            break;
+        case GREEDY_CYCLE:
+            solutions.push_back(run_greedy_cycle(tsp, n, i));
+            break;
+        case GREEDY_CYCLE_REGRET:
+            CycleRegretSolver solver(tsp);
+            solutions.push_back(solver.solve(n, i, 2));
+            break;
+        }
+    }
+
+    return solutions;
 }
---
+++ src/task2/solve_greedy_regret.cpp
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <algorithm>
+#include <climits>
+#include <numeric>
+#include <set>
+
+#include "../common/types.cpp"
+#include "../task1/solve_greedy_cycle.cpp"
+
+unsigned total_cost(const tsp_t &tsp, std::vector<unsigned> path) {
+    unsigned cost = 0;
+    for (int i = 0; i < path.size(); i++) {
+        unsigned curr_node = path[i];
+        unsigned next_node = (i == path.size() - 1) ? path[0] : path[i + 1];
+        cost +=
+            tsp.nodes[curr_node].weight + tsp.adj_matrix(curr_node, next_node);
+    }
+    return cost;
+}
+
+class CycleRegretSolver {
+  private:
+    const tsp_t &tsp;
+    std::set<unsigned> remaining_nodes;
+    solution_t solution;
+
+    void add_starting_cycle(unsigned start) {
+        // Select first 3 nodes "optimally"
+        for (auto i : remaining_nodes) {
+            for (auto j : remaining_nodes) {
+                if (i == j || i == start || j == start) {
+                    continue;
+                }
+
+                int candidate_cost =
+                    tsp.nodes[i].weight + tsp.nodes[j].weight +
+                    tsp.nodes[start].weight + tsp.adj_matrix.m[start][i] +
+                    tsp.adj_matrix.m[i][j] + tsp.adj_matrix.m[j][start];
+
+                if (candidate_cost < solution.cost) {
+                    solution.path = {start, i, j};
+                    solution.cost = candidate_cost;
+                }
+            }
+        }
+        for (auto node : solution.path) {
+            remaining_nodes.erase(node);
+        }
+    }
+
+    void reset() {
+        solution = {INT_MAX, {}};
+        remaining_nodes = std::set<unsigned>();
+        for (int i = 0; i < tsp.n; i++) {
+            remaining_nodes.insert(i);
+        }
+    }
+
+  public:
+    CycleRegretSolver(const tsp_t &tsp) : tsp(tsp) { reset(); }
+
+    solution_t solve(unsigned n, unsigned start, unsigned regret_k) {
+        reset();
+
+        add_starting_cycle(start);
+
+        // Select the remaining nodes using GreedyCycle
+        std::vector<std::pair<unsigned, int>> cost_diffs;
+        cost_diffs.reserve(tsp.n);
+        while (solution.path.size() < n) {
+            int max_regret = INT_MIN;
+            unsigned best_pos = 0;
+            unsigned best_node = 0;
+            for (unsigned node : remaining_nodes) {
+                cost_diffs.clear();
+                for (int pos = 0; pos < solution.path.size(); pos++) {
+                    int diff = get_cost_diff(tsp, solution.path, node, pos);
+                    cost_diffs.push_back({pos, diff});
+                }
+                std::sort(cost_diffs.begin(), cost_diffs.end(),
+                          [](const auto &pair1, const auto &pair2) {
+                              return pair1.second < pair2.second;
+                          });
+                int regret = 0;
+                std::for_each(cost_diffs.begin(), cost_diffs.begin() + regret_k,
+                              [&regret, cost_diffs](
+                                  const std::pair<unsigned, int> &pair) {
+                                  regret += pair.second - cost_diffs[0].second;
+                              });
+                if (regret > max_regret) {
+                    max_regret = regret;
+                    best_node = node;
+                    best_pos = cost_diffs[0].first;
+                }
+            }
+
+            solution.path.insert(solution.path.begin() + best_pos + 1,
+                                 best_node);
+            remaining_nodes.erase(best_node);
+        }
+        solution.cost = total_cost(tsp, solution.path);
+        return solution;
+    }
+};
