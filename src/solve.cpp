6e87dafd954b01c5318175452c79fc33d3a293c6
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-20 23:03:30 +0200

    fix: use dictionary instead of switch in solve.cpp

--- src/solve.cpp
+++ src/solve.cpp
@@ -8,12 +8,12 @@
 #include <vector>
 
 enum heuristic_t {
-    RANDOM,
-    NN_END,
-    NN_ANY,
-    GREEDY_CYCLE,
-    GREEDY_CYCLE_REGRET,
-    GREEDY_CYCLE_REGRET_WEIGHTED
+  RANDOM,
+  NN_END,
+  NN_ANY,
+  GREEDY_CYCLE,
+  GREEDY_CYCLE_REGRET,
+  GREEDY_CYCLE_REGRET_WEIGHTED
 };
 
 std::map<heuristic_t, std::string> heuristic_t_str = {
@@ -24,41 +24,26 @@ std::map<heuristic_t, std::string> heuristic_t_str = {
     {GREEDY_CYCLE_REGRET, "greedy_cycle_regret"},
     {GREEDY_CYCLE_REGRET_WEIGHTED, "greedy_cycle_regret_weighted"}};
 
+std::map<heuristic_t, solution_t (*)(const tsp_t &, unsigned int, unsigned int)>
+    heuristic_t_fn = {{NN_END, solve_nn_end},
+                      {NN_ANY, solve_nn_any},
+                      {GREEDY_CYCLE, solve_greedy_cycle},
+                      {GREEDY_CYCLE_REGRET, solve_regret_unweighted},
+                      {GREEDY_CYCLE_REGRET_WEIGHTED, solve_regret_weighted}};
+
 std::vector<solution_t> solve(const tsp_t &tsp, heuristic_t heuristic) {
-    std::vector<solution_t> solutions;
-    unsigned int n = ceil(tsp.n / 2.0);
+  std::vector<solution_t> solutions;
+  unsigned int n = ceil(tsp.n / 2.0);
 
-    if (heuristic == RANDOM) {
-        return solve_random(tsp, n);
-    }
+  if (heuristic == RANDOM) {
+    return solve_random(tsp, n);
+  }
 
-    for (unsigned int i = 0; i < tsp.n; i++) {
-        switch (heuristic) {
-        case RANDOM:
-            // NOTE: should never reach this point, but it's here to avoid a
-            // warning
-            break;
-        case NN_END:
-            solutions.push_back(solve_nn_end(tsp, n, i));
-            break;
-        case NN_ANY:
-            solutions.push_back(solve_nn_any(tsp, n, i));
-            break;
-        case GREEDY_CYCLE:
-            solutions.push_back(run_greedy_cycle(tsp, n, i));
-            break;
-        case GREEDY_CYCLE_REGRET: {
-            CycleRegretSolver solver(tsp);
-            solutions.push_back(solver.solve(n, i, 2, 1.0));
-            break;
-        }
-        case GREEDY_CYCLE_REGRET_WEIGHTED: {
-            CycleRegretSolver solver(tsp);
-            solutions.push_back(solver.solve(n, i, 2, 0.5));
-            break;
-        }
-        }
-    }
+  auto fn = heuristic_t_fn[heuristic];
 
-    return solutions;
+  for (unsigned int i = 0; i < tsp.n; i++) {
+    solutions.push_back(fn(tsp, n, i));
+  }
+
+  return solutions;
 }
