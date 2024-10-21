7ebe9293bc54357a4712023de66d4020828889cd
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-21 10:08:49 +0200

    fix: fix nn_any to consider prepending

--- src/task1/solve_nn.cpp
+++ src/task1/solve_nn.cpp
@@ -18,7 +18,7 @@ solution_t solve_nn_any(const tsp_t &tsp, unsigned int n, unsigned int start) {
   solution_t solution(tsp, start);
 
   while (solution.path.size() < n) {
-    unsigned int min_pos = 0;
+    unsigned int min_pos = -1;
     auto [min, min_cost] = find_nn(tsp, solution, solution.path.front());
 
     for (int i = 0; i < solution.path.size() - 1; i++) {
@@ -34,7 +34,7 @@ solution_t solve_nn_any(const tsp_t &tsp, unsigned int n, unsigned int start) {
     auto [last, last_cost] = find_nn(tsp, solution, solution.path.back());
     if (last_cost < min_cost) {
       solution.append(last);
-    } else if (min_pos == 0) {
+    } else if (min_pos == -1) {
       solution.prepend(min);
     } else {
       solution.insert(min, min_pos);
