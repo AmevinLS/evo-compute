9592ce23e0797da3a8eae12d496250aa5e49a43f
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-25 21:04:05 +0200

    fix: add type specification in greedy_cycle_regret solver lambdas

--- src/task2/solve_greedy_regret.cpp
+++ src/task2/solve_greedy_regret.cpp
@@ -9,12 +9,14 @@
 #define REGRET_K 2
 #define REGRET_WEIGHT 0.5
 
+typedef std::pair<unsigned int, int> pos_cost_t;
+
 solution_t solve_regret(const tsp_t &tsp, unsigned int n, unsigned int start,
                         float weight) {
 
     solution_t solution(tsp, find_cycle(tsp, start));
 
-    std::vector<std::pair<unsigned int, int>> costs;
+    std::vector<pos_cost_t> costs;
     costs.reserve(n);
 
     while (solution.path.size() < n) {
@@ -30,13 +32,13 @@ solution_t solve_regret(const tsp_t &tsp, unsigned int n, unsigned int start,
             }
 
             std::sort(costs.begin(), costs.end(),
-                      [](const auto &pair1, const auto &pair2) {
+                      [](const pos_cost_t &pair1, const pos_cost_t &pair2) {
                           return pair1.second < pair2.second;
                       });
 
             int regret =
                 std::reduce(costs.begin(), costs.begin() + REGRET_K, 0,
-                            [&](int regret, auto pair) {
+                            [&](int regret, pos_cost_t pair) {
                                 return regret + pair.second - costs[0].second;
                             });
 
