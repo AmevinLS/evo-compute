2cd237fc4edd5faead29a773a91690c1063d2845
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-10-28 00:41:16 +0100

    fix: change old code to new types/search files

--- src/task1/solve_nn.cpp
+++ src/task1/solve_nn.cpp
@@ -5,9 +5,11 @@ solution_t solve_nn_end(const tsp_t &tsp, unsigned int n, unsigned int start) {
     solution_t solution(tsp, start);
 
     while (solution.path.size() < n) {
-        best_t min = find_nn(tsp, solution, solution.path.back());
+        auto [min, min_delta] = find_nn(solution, [=](unsigned int node) {
+            return solution.append_delta(node);
+        });
 
-        solution.append(min.first);
+        solution.append(min);
     }
 
     solution.commit();
@@ -19,21 +21,26 @@ solution_t solve_nn_any(const tsp_t &tsp, unsigned int n, unsigned int start) {
 
     while (solution.path.size() < n) {
         unsigned int min_pos = -1;
-        auto [min, min_cost] = find_nn(tsp, solution, solution.path.front());
+
+        auto [min, min_delta] = find_nn(solution, [=](unsigned int node) {
+            return solution.prepend_delta(node);
+        });
 
         for (int i = 0; i < solution.path.size() - 1; i++) {
-            auto [min_replace, min_replace_cost] =
-                find_replace(tsp, solution, i);
+            auto [min_replace, min_replace_delta] = find_replace(solution, i);
 
-            if (min_replace_cost < min_cost) {
+            if (min_replace_delta < min_delta) {
                 min = min_replace;
-                min_cost = min_replace_cost;
+                min_delta = min_replace_delta;
                 min_pos = i;
             }
         }
 
-        auto [last, last_cost] = find_nn(tsp, solution, solution.path.back());
-        if (last_cost < min_cost) {
+        auto [last, last_delta] = find_nn(solution, [=](unsigned int node) {
+            return solution.append_delta(node);
+        });
+
+        if (last_delta < min_delta) {
             solution.append(last);
         } else if (min_pos == -1) {
             solution.prepend(min);
--- src/task2/solve_greedy_regret.cpp
+++ src/task2/solve_greedy_regret.cpp
@@ -1,23 +1,22 @@
+#pragma once
+
 #include "../common/search.cpp"
 #include "../common/types.cpp"
 
 #include <algorithm>
 #include <climits>
 #include <numeric>
-#include <utility>
 
 #define REGRET_K 2
 #define REGRET_WEIGHT 0.5
-
-typedef std::pair<unsigned int, int> pos_cost_t;
 
 solution_t solve_regret(const tsp_t &tsp, unsigned int n, unsigned int start,
                         float weight) {
 
     solution_t solution(tsp, find_cycle(tsp, start));
 
-    std::vector<pos_cost_t> costs;
-    costs.reserve(n);
+    std::vector<pos_delta_t> deltas;
+    deltas.reserve(n);
 
     while (solution.path.size() < n) {
         std::optional<unsigned int> max;
@@ -25,29 +24,29 @@ solution_t solve_regret(const tsp_t &tsp, unsigned int n, unsigned int start,
         int idx = 0;
 
         for (auto node : solution.remaining_nodes) {
-            costs.clear();
+            deltas.clear();
 
             for (int i = 0; i < solution.path.size(); i++) {
-                costs.push_back({i, solution.get_cost_diff(node, i)});
+                deltas.push_back({i, solution.insert_delta(node, i)});
             }
 
-            std::sort(costs.begin(), costs.end(),
-                      [](const pos_cost_t &pair1, const pos_cost_t &pair2) {
+            std::sort(deltas.begin(), deltas.end(),
+                      [](const pos_delta_t &pair1, const pos_delta_t &pair2) {
                           return pair1.second < pair2.second;
                       });
 
             int regret = std::accumulate(
-                costs.begin(), costs.begin() + REGRET_K, 0,
-                [&](int regret, pos_cost_t pair) {
-                    return regret + pair.second - costs[0].second;
+                deltas.begin(), deltas.begin() + REGRET_K, 0,
+                [&](int regret, pos_delta_t pair) {
+                    return regret + pair.second - deltas[0].second;
                 });
 
-            int score = weight * regret - (1 - weight) * costs[0].second;
+            int score = weight * regret - (1 - weight) * deltas[0].second;
 
             if (score > max_score) {
                 max_score = score;
                 max = node;
-                idx = costs[0].first;
+                idx = deltas[0].first;
             }
         }
 
