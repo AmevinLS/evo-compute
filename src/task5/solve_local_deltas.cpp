8e97c24e57e3a98601076fc5753e564e4e777ccf
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-11-16 00:38:29 +0100

    Refactor: a bit cleaner code

--- src/task5/solve_local_deltas.cpp
+++ src/task5/solve_local_deltas.cpp
@@ -16,31 +16,6 @@ struct edge_t {
     unsigned to;
 };
 
-struct UndirectedEdgeHash {
-    std::size_t operator()(const edge_t &edge) const {
-        unsigned a, b;
-        if (edge.from < edge.to) {
-            a = edge.from;
-            b = edge.to;
-        } else {
-            a = edge.to;
-            b = edge.from;
-        }
-        return std::hash<std::string>()(std::to_string(a) + "," +
-                                        std::to_string(b));
-    }
-};
-
-bool edges_same_direction(const edge_t &edge1, const edge_t &edge2) {
-    if (edge1.from == edge2.from && edge1.to == edge2.to)
-        return true;
-    else if (edge1.from == edge2.to && edge1.to == edge2.from)
-        return false;
-    else
-        throw std::logic_error("'edges_same_direction' shouldn't be called on "
-                               "edges with differing nodes");
-}
-
 inline bool operator==(const edge_t &lhs, const edge_t &rhs) {
     return lhs.from == rhs.from && lhs.to == rhs.to;
 }
@@ -51,8 +26,6 @@ struct oper_info_t {
     edge_t rem_edge2;                 // One of removed edges
     std::optional<unsigned> new_node; // If .has_value(), then the op is
                                       // REPLACE, otherwise - REVERSE
-
-    inline bool is_intra() const { return !new_node.has_value(); }
 };
 
 inline bool operator==(const oper_info_t &lhs, const oper_info_t &rhs) {
@@ -102,34 +75,158 @@ void update_list_with_new_opers(const solution_t &sol, oper_list_t &oper_list) {
     }
 }
 
-std::optional<operation_t> steepest_deltas_search(const solution_t &solution,
-                                                  oper_list_t &oper_pq) {
-    std::optional<oper_info_t> best_op_info;
+inline std::optional<unsigned> get_node_idx(const solution_t &sol,
+                                            unsigned node) {
+    auto iter = std::find(sol.path.cbegin(), sol.path.cend(), node);
+    if (iter == sol.path.cend()) {
+        return std::nullopt;
+    }
+    return iter - sol.path.cbegin();
+}
+
+struct edge_tracker_t {
+    struct UndirectedEdgeHash {
+        std::size_t operator()(const edge_t &edge) const {
+            unsigned a, b;
+            if (edge.from < edge.to) {
+                a = edge.from;
+                b = edge.to;
+            } else {
+                a = edge.to;
+                b = edge.from;
+            }
+            return std::hash<std::string>()(std::to_string(a) + "," +
+                                            std::to_string(b));
+        }
+    };
+
+    enum verdict_t { REMOVE, LEAVE, USE };
 
     std::unordered_set<edge_t, UndirectedEdgeHash> edges_in_sol;
-    for (unsigned i = 1; i < solution.path.size(); i++) {
-        edges_in_sol.insert(
-            edge_t{solution.path[solution.prev(i)], solution.path[i]});
-    }
-    edges_in_sol.insert(edge_t{solution.path.back(), solution.path.front()});
+    const solution_t &solution;
 
+    edge_tracker_t(const solution_t &solution)
+        : edges_in_sol(solution_to_edges(solution)), solution(solution) {}
+
+    std::unordered_set<edge_t, UndirectedEdgeHash>
+    solution_to_edges(const solution_t &solution) {
+        std::unordered_set<edge_t, UndirectedEdgeHash> edge_set;
+        for (unsigned i = 1; i < solution.path.size(); i++) {
+            edge_set.insert(
+                edge_t{solution.path[solution.prev(i)], solution.path[i]});
+        }
+        edge_set.insert(edge_t{solution.path.back(), solution.path.front()});
+        return edge_set;
+    }
+
+    void update() {
+        edges_in_sol = solution_to_edges(solution);
+
+        // edges_in_sol.erase(oper_info.rem_edge1);
+        // edges_in_sol.erase(oper_info.rem_edge2);
+
+        // if (oper_info.new_node.has_value()) {
+        //     edges_in_sol.insert(
+        //         edge_t{oper_info.rem_edge1.from,
+        //         oper_info.new_node.value()});
+        //     edges_in_sol.insert(
+        //         edge_t{oper_info.new_node.value(), oper_info.rem_edge2.to});
+        // } else {
+        //     edges_in_sol.insert(
+        //         edge_t{oper_info.rem_edge1.from, oper_info.rem_edge2.from});
+        //     edges_in_sol.insert(
+        //         edge_t{oper_info.rem_edge1.to, oper_info.rem_edge2.to});
+        // }
+        // if (edges_in_sol.size() != solution.path.size())
+        //     throw std::logic_error("Number of edges different than path
+        //     size");
+    }
+
+    static bool edges_same_direction(const edge_t &edge1, const edge_t &edge2) {
+        if (edge1.from == edge2.from && edge1.to == edge2.to)
+            return true;
+        else if (edge1.from == edge2.to && edge1.to == edge2.from)
+            return false;
+        else
+            throw std::logic_error(
+                "'edges_same_direction' shouldn't be called on "
+                "edges with differing nodes");
+    }
+
+    verdict_t check(const oper_info_t &oper_info) const {
+        // Handle case when new_node already in solution
+        if (oper_info.new_node.has_value() &&
+            solution.remaining_nodes.count(oper_info.new_node.value()) > 0)
+            return REMOVE;
+
+        auto edge_iter1 = edges_in_sol.find(oper_info.rem_edge1);
+        auto edge_iter2 = edges_in_sol.find(oper_info.rem_edge2);
+        // Handle case if at least on of defining edges not present
+        if (edge_iter1 == edges_in_sol.end() ||
+            edge_iter2 == edges_in_sol.end()) {
+            return REMOVE;
+        }
+
+        // Handle case if at least one is in the wrong direction
+        if (!edges_same_direction(*edge_iter1, oper_info.rem_edge1) ||
+            !edges_same_direction(*edge_iter2, oper_info.rem_edge2))
+            return LEAVE;
+
+        // Handle case 1->2->3, and we want to swap 1->2 and 2->3
+        if (oper_info.new_node.has_value() &&
+            (get_node_idx(solution, edge_iter1->to) ==
+                 get_node_idx(solution, edge_iter2->from) ||
+             get_node_idx(solution, edge_iter1->from) ==
+                 get_node_idx(solution, edge_iter2->to)))
+            return LEAVE;
+
+        return USE;
+    }
+};
+
+operation_t convert_info_to_oper(const solution_t &solution,
+                                 const oper_info_t &oper_info) {
+    if (!oper_info.new_node.has_value()) {
+        // This is a REVERSE operation
+        unsigned idx1 = get_node_idx(solution, oper_info.rem_edge1.to).value(),
+                 idx2 =
+                     get_node_idx(solution, oper_info.rem_edge2.from).value();
+        if (idx1 > idx2) {
+            std::swap(idx1, idx2);
+            idx1 = solution.next(idx1);
+            idx2 = solution.prev(idx2);
+        }
+        return operation_t{solution_t::REVERSE, idx1, idx2, oper_info.delta};
+    } else {
+        // This is a REPLACE operation
+        if (oper_info.rem_edge1.to != oper_info.rem_edge2.from)
+            throw std::logic_error("Invalid order of edges in `edge_t`");
+        unsigned pos = std::find(solution.path.cbegin(), solution.path.cend(),
+                                 oper_info.rem_edge1.to) -
+                       solution.path.cbegin();
+        return operation_t{solution_t::REPLACE, oper_info.new_node.value(), pos,
+                           oper_info.delta};
+    }
+}
+
+std::optional<operation_t>
+steepest_deltas_search(const solution_t &solution, oper_list_t &oper_pq,
+                       edge_tracker_t &edge_tracker) {
     // Main loop, iterating through move list
+    std::optional<oper_info_t> best_op_info;
     std::stack<oper_list_t::iterator>
         iters_to_remove; // Stack is to start removing elems from the end
     for (auto iter = oper_pq.begin(); iter != oper_pq.end(); iter++) {
-        auto edge_iter1 = edges_in_sol.find(iter->rem_edge1);
-        auto edge_iter2 = edges_in_sol.find(iter->rem_edge2);
-        if (edge_iter1 == edges_in_sol.end() ||
-            edge_iter2 == edges_in_sol.end()) {
+        edge_tracker_t::verdict_t verdict = edge_tracker.check(*iter);
+        if (verdict == edge_tracker_t::REMOVE) {
             iters_to_remove.push(iter);
             continue;
-        }
-        if (!edges_same_direction(*edge_iter1, iter->rem_edge1) ||
-            !edges_same_direction(*edge_iter2, iter->rem_edge2))
+        } else if (verdict == edge_tracker_t::LEAVE) {
             continue;
-
-        best_op_info = *iter;
-        break;
+        } else if (verdict == edge_tracker_t::USE) {
+            best_op_info = *iter;
+            break;
+        }
     }
 
     // Delete the moves in move list which are marked for deletion
@@ -141,34 +238,17 @@ std::optional<operation_t> steepest_deltas_search(const solution_t &solution,
     if (!best_op_info.has_value()) {
         return std::nullopt;
     }
-
-    if (best_op_info->is_intra()) {
-        unsigned idx1 = std::find(solution.path.cbegin(), solution.path.cend(),
-                                  best_op_info->rem_edge1.to) -
-                        solution.path.cbegin(),
-                 idx2 = std::find(solution.path.cbegin(), solution.path.cend(),
-                                  best_op_info->rem_edge2.from) -
-                        solution.path.cbegin();
-        return operation_t{solution_t::REVERSE, idx1, idx2,
-                           best_op_info->delta};
-    } else {
-        if (best_op_info->rem_edge1.to != best_op_info->rem_edge2.from)
-            throw std::logic_error("Invalid order of edges in `edge_t`");
-        unsigned pos = std::find(solution.path.cbegin(), solution.path.cend(),
-                                 best_op_info->rem_edge1.to) -
-                       solution.path.cbegin();
-        return operation_t{solution_t::REPLACE, best_op_info->new_node.value(),
-                           pos, best_op_info->delta};
-    }
+    return convert_info_to_oper(solution, best_op_info.value());
 }
 
 solution_t local_deltas_steepest(const tsp_t &tsp, solution_t solution) {
     oper_list_t oper_pq;
+    edge_tracker_t edge_tracker(solution);
 
     while (true) {
         update_list_with_new_opers(solution, oper_pq);
         std::optional<operation_t> best_op =
-            steepest_deltas_search(solution, oper_pq);
+            steepest_deltas_search(solution, oper_pq, edge_tracker);
 
         solution.search_iters++;
 
@@ -187,6 +267,7 @@ solution_t local_deltas_steepest(const tsp_t &tsp, solution_t solution) {
             throw std::logic_error("Invalid operation type");
             break;
         }
+        edge_tracker.update();
         if (!solution.is_valid()) {
             throw std::logic_error("Solution is invalid");
         } else if (!solution.is_cost_correct()) {
