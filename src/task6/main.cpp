2383f0c58ecc0ca7e87d6d5cb663278780a1ab6a
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-11-24 20:25:37 +0100

    refactor: iterated cleanup + different constants

--- src/task6/main.cpp
+++ src/task6/main.cpp
@@ -6,10 +6,7 @@
 #include "../common/parse.cpp"
 #include "../common/print.cpp"
 #include "../common/types.cpp"
-#include "../task1/solve_random.cpp"
-#include "../task5/solve_local_deltas.cpp"
 #include "solve_local_iterated.cpp"
-#include "solve_local_multiple.cpp"
 
 int main() {
     std::ifstream fin("../../data/TSPA.csv");
@@ -17,13 +14,12 @@ int main() {
     timer_t timer;
 
     const unsigned NUM_RUNS = 10;
-    const unsigned MSLS_ITERATIONS = 200;
     const unsigned PATH_SIZE = 100;
 
     std::vector<solution_t> solutions_msls;
     for (unsigned i = 0; i < NUM_RUNS; i++) {
         timer.start();
-        solution_t sol = solve_local_multiple_start(tsp, MSLS_ITERATIONS);
+        solution_t sol = solve_local_multiple_start(tsp, PATH_SIZE);
         sol.runtime_ms = timer.measure();
         solutions_msls.push_back(sol);
         std::cout << "MSLS " << i << ": " << sol.cost << "\n";
@@ -39,9 +35,8 @@ int main() {
     std::vector<solution_t> solutions_iter;
     for (unsigned i = 0; i < NUM_RUNS; i++) {
         timer.start();
-        solution_t random_sol = gen_random_solution(tsp, PATH_SIZE);
         solution_t iterated_sol =
-            solve_local_iterated_search(random_sol, avg_msls_duration);
+            solve_local_iterated_search(tsp, PATH_SIZE, avg_msls_duration);
         iterated_sol.runtime_ms = timer.measure();
         solutions_iter.push_back(iterated_sol);
         std::cout << "Iterated " << i << ": " << iterated_sol.cost << "\n";
--- src/task6/solve_local_iterated.cpp
+++ src/task6/solve_local_iterated.cpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <chrono>
 #include <random>
 
 #include "../common/types.cpp"
@@ -14,63 +13,53 @@ int random_num(int start, int end) {
 }
 
 // Perturb the solution in-place
-void perturb_solution(solution_t &sol) {
-    // for (int i = 0; i < 3; i++) {
-    //     auto iter = sol.remaining_nodes.begin();
-    //     std::advance(iter, random_num(0, sol.remaining_nodes.size()));
-    //     sol.replace(*iter, random_num(0, sol.path.size()));
-    // }
-    const unsigned NUM_ALTERATIONS = 10;
-    const unsigned MAX_SHIFT = 5;
-    const double REVERSE_PROB = 0.6, SWAP_PROB = 0.2;
-    for (unsigned i = 0; i < NUM_ALTERATIONS; i++) {
-        unsigned idx1 = random_num(0, sol.path.size());
-        unsigned shift = random_num(0, MAX_SHIFT + 1);
+void perturb_solution(solution_t &solution, unsigned int alterations = 10,
+                      unsigned int max_shift = 15, double reverse_prob = 40,
+                      double swap_prob = 80) {
+    for (unsigned int i = 0; i < alterations; i++) {
+        unsigned int idx1 = random_num(0, solution.path.size());
+        unsigned int shift = random_num(0, max_shift + 1);
+        int prob = random_num(0, 100);
         unsigned idx2 = idx1;
+
         for (unsigned j = 0; j < shift; j++)
-            idx2 = sol.next(idx2);
+            idx2 = solution.next(idx2);
+
         if (idx1 > idx2)
             std::swap(idx1, idx2);
 
-        int prob = random_num(0, 100);
-        if (prob < REVERSE_PROB * 100) {
-            sol.reverse(idx1, idx2);
-        } else if (prob < (REVERSE_PROB + SWAP_PROB) * 100) {
-            sol.swap(idx1, idx2);
+        if (prob < reverse_prob) {
+            solution.reverse(idx1, idx2);
+        } else if (prob < swap_prob) {
+            solution.swap(idx1, idx2);
         } else {
-            auto iter = sol.remaining_nodes.begin();
-            std::advance(iter, random_num(0, sol.remaining_nodes.size()));
-            sol.replace(*iter, random_num(0, sol.path.size()));
+            auto iter = solution.remaining_nodes.begin();
+            std::advance(iter, random_num(0, solution.remaining_nodes.size()));
+            solution.replace(*iter, random_num(0, solution.path.size()));
         }
     }
 }
 
-solution_t solve_local_iterated_search(solution_t solution,
-                                       unsigned time_limit_ms) {
+solution_t solve_local_iterated_search(const tsp_t &tsp, unsigned int path_size,
+                                       unsigned int time_limit_ms) {
     int best_cost = INT_MAX;
-    solution_t best_sol = solution;
-    solution_t curr_sol = solution;
+    solution_t solution = gen_random_solution(tsp, path_size);
+    solution_t best = solution;
+    timer_t timer;
 
-    const std::chrono::time_point start_time =
-        std::chrono::high_resolution_clock::now();
-    while (true) {
-        curr_sol = solve_local_search(curr_sol, solution_t::REVERSE, STEEPEST);
+    timer.start();
+    while (timer.measure() < time_limit_ms) {
+        solution = solve_local_search(solution, solution_t::REVERSE, STEEPEST);
 
-        const std::chrono::time_point curr_time =
-            std::chrono::high_resolution_clock::now();
-        int curr_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                          curr_time - start_time)
-                          .count();
-        if (curr_ms > time_limit_ms)
-            break;
-
-        if (curr_sol.cost < best_cost) {
-            best_cost = curr_sol.cost;
-            best_sol = curr_sol;
+        if (solution.cost < best_cost) {
+            best_cost = solution.cost;
+            best = solution;
         }
-        perturb_solution(curr_sol);
+
+        perturb_solution(solution);
     }
-    return curr_sol;
+
+    return best;
 }
 
 solution_t solve_local_multiple_start(const tsp_t &tsp, unsigned int n) {
