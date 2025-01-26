7c196dc75e55fa5a1f623e5a1bf3419864838c21
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-01-26 22:58:08 +0100

    refactor: include parsing file into parsing tsp

--- src/common/parse.cpp
+++ src/common/parse.cpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#include "print.cpp"
+#include "types.cpp"
+
 #include <algorithm>
 #include <cmath>
 #include <fstream>
+#include <iostream>
 #include <numeric>
+#include <optional>
 #include <sstream>
 #include <string>
 #include <vector>
-
-#include "types.cpp"
 
 int l2(const node_t a, const node_t b) {
     return int(round(sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))));
@@ -46,6 +49,18 @@ adj_matrix_t matrixof(const std::vector<node_t> nodes) {
     return matrix;
 }
 
+std::optional<std::ifstream> open_file(const std::string &fname) {
+    std::ifstream in(fname);
+    if (!in.is_open()) {
+        std::cerr << ERROR << " failed to open file: " << fname << std::endl;
+        std::cerr
+            << "Check if the file exists and you have permission to read it."
+            << std::endl;
+        return {};
+    }
+    return in;
+}
+
 std::vector<node_t> read(std::ifstream &in) {
     std::vector<node_t> nodes;
     std::string line;
@@ -68,8 +83,20 @@ std::vector<node_t> read(std::ifstream &in) {
     return nodes;
 }
 
-tsp_t parse(std::ifstream &in) {
-    std::vector<node_t> nodes = read(in);
+std::optional<tsp_t> parse(const std::string &fname) {
+    auto in = open_file(fname);
+
+    if (!in.has_value()) {
+        return std::nullopt;
+    }
+
+    std::vector<node_t> nodes = read(in.value());
     adj_matrix_t matrix = matrixof(nodes);
     return tsp_t(nodes, matrix);
+}
+
+std::string instance_name(const std::string &fname) {
+    std::string instance_name = fname.substr(fname.find_last_of("/\\") + 1);
+    instance_name = instance_name.substr(0, instance_name.find_last_of("."));
+    return instance_name;
 }
