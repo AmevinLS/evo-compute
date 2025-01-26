8727946a2a70faab3b921fb05bb3ce3209e7a2d3
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-01-26 23:05:28 +0100

    refactor: move steepest and greedy search into common/search

--- src/common/search.cpp
+++ src/common/search.cpp
@@ -5,6 +5,7 @@
 #include <climits>
 #include <functional>
 #include <optional>
+#include <random>
 #include <vector>
 
 node_delta_t find_nn(const solution_t &solution,
@@ -66,4 +67,76 @@ std::vector<unsigned int> find_cycle(const tsp_t &tsp, unsigned int start) {
     }
 
     return path.value();
+}
+
+std::optional<operation_t>
+greedy_search(const solution_t &solution,
+              std::vector<operation_t> &neighbourhood,
+              std::vector<int> &indices, op_type_t op_type) {
+    neighbourhood.clear();
+
+    for (unsigned int i = 0; i < solution.path.size(); i++) {
+        for (unsigned int j = i + 1; j < solution.path.size(); j++) {
+            neighbourhood.emplace_back(operation_t{op_type, i, j, 0});
+        }
+
+        for (auto node : solution.remaining_nodes) {
+            neighbourhood.emplace_back(operation_t{REPLACE, node, i, 0});
+        }
+    }
+
+    std::mt19937 g = std::mt19937(std::random_device()());
+    std::shuffle(indices.begin(), indices.end(), g);
+
+    for (int idx : indices) {
+        operation_t op = neighbourhood[idx];
+        int delta = 0;
+        switch (op.type) {
+        case SWAP:
+            delta = solution.swap_delta(op.arg1, op.arg2);
+            break;
+        case REVERSE:
+            delta = solution.reverse_delta(op.arg1, op.arg2);
+            break;
+        case REPLACE:
+            delta = solution.replace_delta(op.arg1, op.arg2);
+            break;
+        default:
+            break;
+        }
+
+        if (delta < 0) {
+            return op;
+        }
+    }
+
+    return std::nullopt;
+}
+
+std::optional<operation_t> steepest_search(const solution_t &solution,
+                                           op_type_t op_type) {
+    int delta = 0;
+    std::optional<operation_t> best_op;
+
+    for (unsigned int i = 0; i < solution.path.size(); i++) {
+        for (unsigned int j = i + 1; j < solution.path.size(); j++) {
+            int op_delta = op_type == SWAP ? solution.swap_delta(i, j)
+                                           : solution.reverse_delta(i, j);
+            if (op_delta < delta) {
+                delta = op_delta;
+                best_op = operation_t{op_type, i, j, op_delta};
+            }
+        }
+
+        for (auto node : solution.remaining_nodes) {
+            int op_delta = solution.replace_delta(node, i);
+
+            if (op_delta < delta) {
+                delta = op_delta;
+                best_op = operation_t{REPLACE, node, i, delta};
+            }
+        }
+    }
+
+    return best_op;
 }
