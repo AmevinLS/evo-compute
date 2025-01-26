4a9c55dde4dd0c640836a9e2359b405eb7835e54
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-01-26 23:02:50 +0100

    fix: update print methods, move ERROR

--- src/common/random.cpp
+++ src/common/random.cpp
@@ -1,20 +1,28 @@
 #pragma once
 
+#include "types.cpp"
 #include <random>
 #include <vector>
 
 int random_num(int start, int end) {
-    std::random_device rd;  // obtain a random number from hardware
-    std::mt19937 gen(rd()); // seed the generator
-    std::uniform_int_distribution<> d(start, end - 1); // define the range
-    return d(gen);
+    std::mt19937 g((std::random_device()()));
+    std::uniform_int_distribution<> d(start, end - 1);
+    return d(g);
 }
 
 int random_num(const std::vector<int> &weights) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    std::mt19937 g((std::random_device()()));
     std::discrete_distribution<> d(weights.cbegin(), weights.cend());
-    return d(gen);
+    return d(g);
+}
+
+solution_t gen_random_solution(const tsp_t &tsp) {
+    list_t nodes(tsp.n);
+    std::iota(nodes.begin(), nodes.end(), 0);
+
+    std::mt19937 gen((std::random_device()()));
+    std::shuffle(nodes.begin(), nodes.end(), gen);
+    return solution_t(tsp, {nodes.begin(), nodes.begin() + tsp.path_size});
 }
 
 int random_num(const std::vector<double> &weights) {
